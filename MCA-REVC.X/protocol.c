485645fedd46d2848ae63d1fd2a7fd13e5635f58
Miroslav Bozic <mbozic84@gmail.com>
2015-11-24 23:34:24 +0100

    protocol.h and protocol.c files added to the project

---
+++ MCA-REVC.X/protocol.c
@@ -0,0 +1,9 @@
+/******************************************************************************/
+/* File:   protocol.c                                                         */
+/* Author: Miroslav Bozic                                                     */
+/*                                                                            */
+/*High level definition of a protocol messages and message parsing            */
+/******************************************************************************/
+
+#include "protocol.h"
+
