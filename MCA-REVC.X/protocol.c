d03d31b548da3a1b11b889228f642b23ff461068
Miroslav Bozic <mbozic84@gmail.com>
2015-12-05 16:13:08 +0100

    input.c and input.h are added.

--- MCA-REVC.X/protocol.c
+++ MCA-REVC.X/protocol.c
@@ -2,7 +2,7 @@
 /* File:   protocol.c                                                         */
 /* Author: Miroslav Bozic                                                     */
 /*                                                                            */
-/*High level definition of a protocol messages and message parsing            */
+/* High level definition of a protocol messages and message parsing           */
 /******************************************************************************/
 
 #include "protocol.h"
